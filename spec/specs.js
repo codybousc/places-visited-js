e086d8321f6f83f8f04b5800d9589ce9f6b5b288
cody bouscaren & franz knupfer <student@epicodus.com>
2015-06-01 11:35:33 -0700

    address book template complete

---
+++ spec/specs.js
@@ -0,0 +1,8 @@
+
+describe('wordReplace', function() {
+  it("remove's a user provided word from a user provided sentence", function() {
+    expect(wordReplace("Tbone walks on water", "water", "")).to.equal("Tbone walks on ");
+  });
+
+
+});
